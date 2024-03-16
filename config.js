85c9356546da261707f8cf87f4cddcfa7f844e4b
XosuperiorXo <157197548+Lexus-X0@users.noreply.github.com>
2024-03-16 14:04:49 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,19 +8,17 @@ const toBool = (x) => x == "true";
 DATABASE_URL = process.env.DATABASE_URL || "./lib/database.db";
 let HANDLER = "false";
 module.exports = {
-  SPARKY_API: "https://api-aswin-sparky.koyeb.app",
   ANTILINK: toBool(process.env.ANTI_LINK) || false,
   LOGS: toBool(process.env.LOGS) || true,
   ANTILINK_ACTION: process.env.ANTI_LINK || "kick",
   SESSION_ID:process.env.SESSION_ID || " ",
   LANG: process.env.LANG || "EN",
-  HANDLERS: process.env.PREFIX || '^[.]',
+  HANDLERS: process.env.PREFIX || '.',
   BRANCH: "master",
-  STICKER_DATA: process.env.STICKER_DATA || "𝐙𝐞𝐭𝐚-𝐗𝐃;𝐁𝐚𝐝𝐚𝐧 𝐬𝐞𝐫⚇",
-  BOT_INFO: process.env.BOT_INFO || "𝐄𝐳𝐫𝐚𝐡𝐡-𝐗𝐃;𝐙𝐞𝐭𝐚𝐡𝐡;https://i.imgur.com/nAkM1zV.mp4",
-  AUDIO_DATA: process.env.AUDIO_DATA || "𝐳𝐞𝐭𝐚𝐚𝐡𝐡;𝐞𝐳𝐫𝐚𝐚𝐡𝐡-𝐱𝐝;https://i.imgur.com/Ou56ggv.jpeg",
-  ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "false",
-  CAPTION: process.env.CAPTION || "𝐳𝐞𝐭𝐚𝐚𝐡𝐡👁️!",
+  STICKER_DATA: process.env.STICKER_DATA || "HILUX BOT;kazhuveri",
+  BOT_INFO: process.env.BOT_INFO || "HILUX BOT;Kazhuveri",
+  AUDIO_DATA: process.env.AUDIO_DATA || "𝘩𝘰𝘭𝘶𝘹 𝘮𝘥 𝘣𝘰𝘵;𝘬𝘢𝘻𝘩𝘶𝘷𝘦𝘳𝘪",
+  ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "true",
   WORK_TYPE: process.env.WORK_TYPE || "private",
   DATABASE_URL: DATABASE_URL,
   DATABASE:
