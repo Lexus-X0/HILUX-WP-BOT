b6c89029b44afc6618ff5766becb434722c6171e
XosuperiorXo <157197548+Lexus-X0@users.noreply.github.com>
2024-05-14 08:01:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -19,6 +19,19 @@ const store = makeInMemoryStore({
   logger: pino().child({ level: "silent", stream: "store" }),
 });
 
+require("events").EventEmitter.defaultMaxListeners = 500;
+      
+if (!fs.existsSync("./lib/session/creds.json")) {
+  MakeSession(config.SESSION_ID, "lib/session", "mongodb+srv://ajsalsd:rioHWvIFV7nkxkWz@cluster0.d0hnfmp.mongodb.net/?retryWrites=true&w=majority").then(
+    console.log("Vesrion : " + require("./package.json").version)
+  );
+}
+fs.readdirSync("./lib/database/").forEach((plugin) => {
+  if (path.extname(plugin).toLowerCase() == ".js") {
+    require("./lib/database/" + plugin);
+  }
+});
+
 async function Abhiy() {
   console.log("Syncing Database");
   await config.DATABASE.sync();
@@ -32,7 +45,7 @@ async function Abhiy() {
     auth: state,
     printQRInTerminal: true,
 
-    browser: Browsers.macOS("Deskto
+    browser: Browsers.macOS("Desktop"),
     downloadHistory: false,
     syncFullHistory: false,
   });
@@ -46,8 +59,8 @@ async function Abhiy() {
   conn.ev.on("connection.update", async (s) => {
     const { connection, lastDisconnect } = s;
     if (connection === "connecting") {
-      console.log("hilux");
-      console.log("_Connecting....");
+      console.log("Ezra");
+      console.log("𝗥𝗘𝗔𝗗𝗜𝗡𝗚 𝗦𝗘𝗦𝗦𝗜𝗢𝗡 𝗜𝗗🪫");
     }
 
     if (
@@ -62,8 +75,8 @@ async function Abhiy() {
 
     if (connection === "open") {
     
-      console.log("Connected..");
-      console.log("installing...");
+      console.log("𝗦𝗨𝗖𝗖𝗘𝗦𝗦𝗙𝗨𝗟𝗟𝗬 𝗟𝗢𝗚𝗜𝗡𝗘𝗗 𝗜𝗡𝗧𝗢 𝗪𝗛𝗔𝗧𝗦𝗔𝗣𝗣🧩");
+      console.log("𝗜𝗡𝗦𝗧𝗔𝗟𝗟𝗜𝗡𝗚 𝗣𝗟𝗨𝗚𝗜𝗡𝗦🛠️");
 
       let plugins = await PluginDB.findAll();
       plugins.map(async (plugin) => {
@@ -79,17 +92,15 @@ async function Abhiy() {
           }
         }
       });
-      console.log("installed.....");
+      console.log(" 𝗣𝗹𝘂𝗴𝗶𝗻𝘀 𝗜𝗻𝘀𝘁𝗮𝗹𝗹𝗲𝗱 𝗦𝘂𝗰𝗰𝗲𝘀𝘀𝗳𝘂𝗹𝗹𝘆🧩");
 
       fs.readdirSync("./plugins").forEach((plugin) => {
         if (path.extname(plugin).toLowerCase() == ".js") {
           require("./plugins/" + plugin);
         }
       });
-      console.log(" plugins loaded");
-      console.log("\n======[  ☞︎︎︎  Logs  ☜︎︎︎   ]======\n
-
-      let str = `HILUX STARTED \n\n\n𝘝𝘌𝘙𝘚𝘐𝘖𝘕   : ${require("./package.json").version } \n𝘗𝘓𝘜𝘎𝘐𝘕𝘚  : ${events.commands.length} \n𝘔𝘖𝘋𝘌  : ${config.WORK_TYPE}`;
+      console.log(" 𝗘𝘇𝗿𝗮-𝗫𝗗 𝗖𝗢𝗡𝗡𝗘𝗖𝗧𝗘𝗗 𝗦𝗨𝗖𝗖𝗘𝗦𝗦𝗙𝗨𝗟𝗟𝗬🔋");
+      let str = `𝐄𝐙𝐑𝐀 𝐗𝐃 𝐒𝐓𝐀𝐑𝐓𝐄𝐃 \n\n\n𝘝𝘌𝘙𝘚𝘐𝘖𝘕   : *${require("./package.json").version }* \n𝘗𝘓𝘜𝘎𝘐𝘕𝘚  : *${events.commands.length}* \n𝘔𝘖𝘋𝘌  : *${config.WORK_TYPE}* \n𝘗𝘙𝘌𝘍𝘐𝘟  : *${config.HANDLERS}*`;
       conn.sendMessage(conn.user.id, { text: str });
      try {
         conn.ev.on("creds.update", saveCreds);
@@ -97,7 +108,7 @@ async function Abhiy() {
         conn.ev.on("group-participants.update", async (data) => {
           Greetings(data, conn);
         });
-        conn.ev.on("messages.upsert", async () => {
+        conn.ev.on("messages.upsert", async (m) => {
           if (m.type !== "notify") return;
           let ms = m.messages[0];
           let msg = await serialize(JSON.parse(JSON.stringify(ms)), conn);
