9f0dd3bea6229e47f91ae94762f92626b8bde858
XosuperiorXo <157197548+MAD-MAX77@users.noreply.github.com>
2024-06-08 00:04:59 +0530

    KADAYADI MWONE

--- plugins/group.js
+++ plugins/group.js
@@ -5,13 +5,6 @@ const {
   toggleFilter,
 } = require("../lib/database/filters");
 const { command, isPrivate, tiny } = require("../lib");
-const {
-  setMessage,
-  getMessage,
-  delMessage,
-  getStatus,
-  toggleStatus,
-} = require("../lib/database/greetings");
 const config = require("../config");
 const { isAdmin, parsedJid, isUrl } = require("../lib");
 const { cron, saveSchedule } = require("../lib/scheduler");
@@ -39,12 +32,12 @@ command(
   async (message, match) => {
     if (!message.isGroup) return await message.reply("_This Group Only Command_")
     let num = match || message.reply_message.jid
-    if (!num) return await message.reply("_give add number or mention him_");
+    if (!num) return await message.reply("_give  number or mention person_");
     let user = num.replace(/[^0-9]/g, "") + "@s.whatsapp.net"
     let admin = await isAdmin(message.jid, message.user, message.client);
-    if (!admin) return await message.reply("*_Add Me Admin First_");
+    if (!admin) return await message.reply("_Add Me Admin First_");
     await message.client.groupParticipantsUpdate(message.jid, [user], "add")
-    return await message.client.sendMessage(message.jid, { text: `_@${user.split("@")[0]} Congratulations this person added this group_`, mentions: [user] })
+    return await message.client.sendMessage(message.jid, { text: `_@${user.split("@")[0]} welcome duplicate indian kazhuverida Mwone_`, mentions: [user] })
   }
 );
 
@@ -59,11 +52,41 @@ command(
   async (message, match) => {
     if (!message.isGroup) return await message.reply("_This group only command_")
     let num = match || message.reply_message.jid
-    if (!num) return await message.reply("_give add number or mention him_");
+    if (!num) return await message.reply("_give number or mention person_");
     let user = num.replace(/[^0-9]/g, "") + "@s.whatsapp.net"
     let admin = await isAdmin(message.jid, message.user, message.client);
-    if (!admin) return await message.reply("_Add Me admin first");
+    if (!admin) return await message.reply("_Add Me admin first_");
     await message.client.groupParticipantsUpdate(message.jid, [user], "remove")
-    return await message.client.sendMessage(message.jid, { text: `_@${user.split("@")[0]}, Kicked from this group_`, mentions: [user] })
+    return await message.client.sendMessage(message.jid, { text: `_@${user.split("@")[0]}, Odu myre kandam vazhi_`, mentions: [user] })
   }
 );
+command(
+    {
+        pattern: "ginfo",
+        fromMe: isPrivate,
+        desc: "group infp",
+        type: "group",
+    },
+    async (message, match, client, m) => {
+        if (!match || !match.match(/^https:\/\/chat\.whatsapp\.com\/[a-zA-Z0-9]/)) return await message.reply("_Need A Group Link_");
+let urlArray = (match).trim().split("/")[3];
+	const metadata = await message.client.groupGetInviteInfo(urlArray)
+const pari = "╭───────────┈⊷\n    _GROUP INFO_\n╰───────────┈⊷\n╭───────────┈⊷\n │═ ⪼ _Gname_ : " + metadata.subject + "\n │═ ⪼ _size_ : " + metadata.size + "\n │═ ⪼ _creator_ : " + (metadata.owner ? metadata.owner.split('@')[0] : 'unknown') + "\n │═ ⪼ _created on_ : " + require('moment-timezone')(metadata.creation * 1000).tz('Asia/Kolkata').format('DD/MM/YYYY') + "\n╰───────────┈⊷";
+return await message.client.sendMessage(message.jid,{ text : (pari)}, {quoted: message })
+    }
+    );
+
+command(
+      { 
+        pattern: "gjid",
+        fromMe: isPrivate,
+        desc: "group jid",
+        type: "group",
+      },
+      async (message, match, client, m) => {
+          if (!match || !match.match(/^https:\/\/chat\.whatsapp\.com\/[a-zA-Z0-9]/)) return await message.reply("_Need A Group Link_");
+let urlArray = (match).trim().split("/")[3];
+	const metadata = await message.client.groupGetInviteInfo(urlArray)
+const andi = "╭───────────┈⊷\n    _GROUP JId_\n╰───────────┈⊷\n╭───────────┈⊷\n │═ ⪼:"+ metadata.id + "\n╰───────────┈⊷ ";
+return await message.client.sendMessage(message.jid,{ text : (andi)},{quoted:message})
+  });
