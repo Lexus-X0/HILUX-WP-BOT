3f971024682edbf1a964da80848e3a4cbb1c362a
XosuperiorXo <157197548+MAD-MAX77@users.noreply.github.com>
2024-06-08 00:10:40 +0530

    Update heroku.js

--- plugins/heroku.js
+++ plugins/heroku.js
@@ -10,12 +10,6 @@ const git = simpleGit();
 const exec = require("child_process").exec;
 const { SUDO } = require("../config");
 
-/* Copyright (C) 2022 X-Electra.
-Licensed under the  GPL-3.0 License;
-you may not use this file except in compliance with the License.
-X-Asena - X-Electra
-*/
-
 command(
   {
     pattern: "restart",
@@ -25,146 +19,13 @@ command(
     type: "heroku",
   },
   async (message) => {
-    await message.sendMessage(`*_Restarting!_*`);
+    await message.sendMessage(`_Restarting!_`);
     await heroku.delete(baseURI + "/dynos").catch(async (error) => {
       await message.sendMessage(`HEROKU : ${error.body.message}`);
     });
   }
 );
 
-/* Copyright (C) 2022 X-Electra.
-Licensed under the  GPL-3.0 License;
-you may not use this file except in compliance with the License.
-X-Asena - X-Electra
-*/
-
-command(
-  {
-    pattern: "shutdown",
-    fromMe: true,
-    type: "heroku",
-    desc: "Dyno off",
-    type: "heroku",
-  },
-  async (message) => {
-    await heroku
-      .get(baseURI + "/formation")
-      .then(async (formation) => {
-        await message.sendMessage(`*_Shutting down._*`);
-        await heroku.patch(baseURI + "/formation/" + formation[0].id, {
-          body: {
-            quantity: 0,
-          },
-        });
-      })
-      .catch(async (error) => {
-        await message.sendMessage(`HEROKU : ${error.body.message}`);
-      });
-  }
-);
-
-/* Copyright (C) 2022 X-Electra.
-Licensed under the  GPL-3.0 License;
-you may not use this file except in compliance with the License.
-X-Asena - X-Electra
-*/
-
-command(
-  {
-    pattern: "dyno",
-    fromMe: isPrivate,
-    desc: "Show Quota info",
-    type: "heroku",
-  },
-  async (message) => {
-    try {
-      heroku
-        .get("/account")
-        .then(async (account) => {
-          const url = `https://api.heroku.com/accounts/${account.id}/actions/get-quota`;
-          headers = {
-            "User-Agent": "Chrome/80.0.3987.149 Mobile Safari/537.36",
-            Authorization: "Bearer " + Config.HEROKU_API_KEY,
-            Accept: "application/vnd.heroku+json; version=3.account-quotas",
-          };
-          const res = await got(url, { headers });
-          const resp = JSON.parse(res.body);
-          const total_quota = Math.floor(resp.account_quota);
-          const quota_used = Math.floor(resp.quota_used);
-          const remaining = total_quota - quota_used;
-          const quota = `Total Quota : ${secondsToDHMS(total_quota)}
-Used  Quota : ${secondsToDHMS(quota_used)}
-Remaning    : ${secondsToDHMS(remaining)}`;
-          await message.sendMessage("```" + quota + "```");
-        })
-        .catch(async (error) => {
-          return await message.sendMessage(`HEROKU : ${error.body.message}`);
-        });
-    } catch (error) {
-      await message.sendMessage(error);
-    }
-  }
-);
-
-/* Copyright (C) 2022 X-Electra.
-Licensed under the  GPL-3.0 License;
-you may not use this file except in compliance with the License.
-X-Asena - X-Electra
-*/
-
-
-
-/* Copyright (C) 2022 X-Electra.
-Licensed under the  GPL-3.0 License;
-you may not use this file except in compliance with the License.
-X-Asena - X-Electra
-*/
-
-
-
-/* Copyright (C) 2022 X-Electra.
-Licensed under the  GPL-3.0 License;
-you may not use this file except in compliance with the License.
-X-Asena - X-Electra
-*/
-
-
-
-/* Copyright (C) 2022 X-Electra.
-Licensed under the  GPL-3.0 License;
-you may not use this file except in compliance with the License.
-X-Asena - X-Electra
-*/
-
-command(
-  {
-    pattern: "allvar",
-    fromMe: true,
-    type: "heroku",
-    desc: "Heroku all env",
-    type: "heroku",
-  },
-  async (message) => {
-    let msg = "```Here your all Heroku vars\n\n\n";
-    heroku
-      .get(baseURI + "/config-vars")
-      .then(async (keys) => {
-        for (const key in keys) {
-          msg += `${key} : ${keys[key]}\n\n`;
-        }
-        return await message.sendMessage(msg + "```");
-      })
-      .catch(async (error) => {
-        await message.sendMessage(`HEROKU : ${error.body.message}`);
-      });
-  }
-);
-
-/* Copyright (C) 2022 X-Electra.
-Licensed under the  GPL-3.0 License;
-you may not use this file except in compliance with the License.
-Louis-X0 - Zeta-X0
-*/
 
 command(
   {
@@ -181,14 +42,14 @@ command(
         Config.BRANCH + "..origin/" + Config.BRANCH,
       ]);
       if (commits.total === 0) {
-        return await message.sendMessage("*_Bot Is Up-to-Date_*");
+        return await message.sendMessage("_Updates Available_");
       } else {
-        await message.reply("*_Update Started_*");
+        await message.reply("_Update Started_");
 
         try {
           var app = await heroku.get("/apps/" + Config.HEROKU_APP_NAME);
         } catch {
-          await message.sendMessage("_Invalid Heroku Details_");
+          await message.sendMessage("_Myr Invalid Heroku Details_");
           await new Promise((r) => setTimeout(r, 1000));
         }
 
@@ -207,21 +68,21 @@ command(
         }
         await git.push("heroku", Config.BRANCH);
 
-        await message.sendMessage("*_Successfully Updated_*");
+        await message.sendMessage("_Successfully Ellavarum Adich keri Va_");
       }
     }
     await git.fetch();
     var commits = await git.log([Config.BRANCH + "..origin/" + Config.BRANCH]);
     if (commits.total === 0) {
-      await message.sendMessage("*_No Updates Available_*");
+      await message.sendMessage("_Oru Kunnayum Available Alla_");
     } else {
-      var availupdate = "*ᴜᴘᴅᴀᴛᴇs ᴀʀᴇ ᴀᴠᴀɪʟᴀʙʟᴇ* \n\n";
+      var availupdate = "_myre update cheyy_\n\n";
       commits["all"].map((commit, num) => {
         availupdate += num + 1 + " ⋆ " + (commit.message) + "\n";
       });
       return await message.client.sendMessage(message.jid, {
         text: availupdate,
-        footer: ("click here to update"),
+        footer: ("_click here to update_"),
       });
     }
   }
@@ -238,67 +99,4 @@ command(
     desc: "Updates the bot",
   },
   async (message) => {}
-);
-
-//Credits Zeta-X0
-//created by Abhiy
-
-command(
-  { pattern: "getsudo ?(.*)", 
-    fromMe: isPrivate, 
-    desc: "shows sudo numbers", 
-    type: "heroku" 
-  },
-  async (message, match, mm) => {
-    const vars = await heroku
-      .get(baseURI + "/config-vars")
-      .catch(async (error) => {
-        return await message.send("HEROKU : " + error.body.message);
-      });
-    await message.sendMessage("```" + `SUDO number are : ${config.SUDO}` + "```");
-  }
-);
-
-command(
-  { pattern: "setsudo ?(.*)", fromMe: isPrivate, desc: "set new sudo", type: "heroku" },
-  async (message, mm) => {
-    var newSudo = (message.reply_message ? message.reply_message.jid : "" || mm).split(
-      "@"
-    )[0];
-    if (!newSudo)
-      return await message.sendMessage("*Need reply/mention/number*", { quoted: message });
-    var setSudo = (SUDO + "," + newSudo).replace(/,,/g, ",");
-    setSudo = setSudo.startsWith(",") ? setSudo.replace(",", "") : setSudo;
-    await message.sendMessage("```new sudo numbers are: ```" + setSudo, {
-      quoted: message,
-    });
-    await message.sendMessage("_It takes 30 seconds to make effect_", { quoted: message });
-    await heroku
-      .patch(baseURI + "/config-vars", { body: { SUDO: setSudo } })
-      .then(async (app) => {});
-  }
-);
-
-command(
-  {
-    pattern: "delsudo ?(.*)",
-    fromMe: isPrivate,
-    desc: "delete sudo number",
-    type: "heroku",
-  },
-  async (message, mm) => {
-    var newSudo = (message.reply_message ? message.reply_message.jid : "" || mm).split(
-      "@"
-    )[0];
-    if (!newSudo) return await message.sendMessage("*Need reply/mention/number*");
-    var setSudo = SUDO.replace(newSudo, "").replace(/,,/g, ",");
-    setSudo = setSudo.startsWith(",") ? setSudo.replace(",", "") : setSudo;
-    await message.sendMessage("```new sudo numbers are: ```" + setSudo, {
-      quoted: message,
-    });
-    await message.sendMessage("_It takes 30 seconds to make effect_", { quoted: message });
-    await heroku
-      .patch(baseURI + "/config-vars", { body: { SUDO: setSudo } })
-      .then(async (app) => {});
-  }
 );
