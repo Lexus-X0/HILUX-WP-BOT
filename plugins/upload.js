1e9ef20326e57c80ec4ceb9aff0bbbf8d0a7fc4e
XosuperiorXo <157197548+MAD-MAX77@users.noreply.github.com>
2024-06-08 00:20:09 +0530

    Update upload.js

--- plugins/upload.js
+++ plugins/upload.js
@@ -1,26 +1,77 @@
-const { command, isPrivate } = require("../lib/");
-const { CAPTION } = require("../config");
-const X = require("../config");
+const {
+  Function,
+  command,
+  qrcode,
+  webp2mp4,
+  isUrl,
+  isPrivate,
+  getJson,
+  getUrl,
+  isIgUrl,
+  findMusic,
+} = require("../lib/");
+const { yta, ytIdRegex, ytv } = require("../lib/yotube");
+const { search } = require("yt-search");
+let gis = require("g-i-s");
+const { AddMp3Meta } = require("../lib");
+const googleTTS = require('google-tts-api')
+const jimp = require("jimp");
+const QRReader = require("qrcode-reader");
+const { RMBG_KEY } = require("../config");
+let { unlink } = require("fs/promises");
+const got = require("got");
+const FormData = require("form-data");
+const stream = require("stream");
+const { promisify } = require("util");
+const pipeline = promisify(stream.pipeline);
+const fs = require("fs");
+const fetch = require("node-fetch");
+const config = require("../config");
 
+Function(
+  {
+    pattern: "img",
+    fromMe: isPrivate,
+    desc: "Google Image search",
+    type: "downloader",
+  },
+  async (message, match) => {
+    if (!match) return await message.sendMessage("_Enter Search name,number_");
+    let [query, amount] = match.split(",");
+    let result = await gimage(query, amount);
+    await message.sendMessage(
+      `_Downloading ${amount || 5} images for ${query}_`
+    );
+    for (let i of result) {
+      await message.sendFromUrl(i);
+    }
+  }
+);
+
+async function gimage(query, amount = 5) {
+  let list = [];
+  return new Promise((resolve, reject) => {
+    gis(query, async (error, result) => {
+      for (
+        var i = 0;
+        i < (result.length < amount ? result.length : amount);
+        i++
+      ) {
+        list.push(result[i].url);
+        resolve(list);
+      }
+    });
+  });
+}
 command(
     {
-        pattern: "upload",
+        pattern: "media",
         fromMe: isPrivate,
-        desc: "Downloads & uploads media from raw URL",
+        desc: "Download media from URL",
         type: "downloader",
     },
     async (message, match) => {
 match = match || message.reply_message.text
-if (!match)return message.reply(`*_Need a imgur/graph Link_*`)
-return await message.sendFromUrl(match, { contextInfo: { externalAdReply: {
-title: "𝐄𝐙𝐑𝐀-𝐗𝐃",
-body: ``,
-sourceUrl: "",
-mediaUrl: "",
-mediaType: 1,
-showAdAttribution: true,
-renderLargerThumbnail: false,
-thumbnailUrl: "https://i.imgur.com/Ou56ggv.jpeg" }}, caption: (X.CAPTION)}, {quoted: message})
+if (!match)return message.reply(`_Need a imgur Link_`)
+return await message.sendFromUrl(match), {quoted: message}
 });
-
-// Zeta-XD
