b9e78dea7194a2a8461179cfabef52fdf30115fd
XosuperiorXo <157197548+MAD-MAX77@users.noreply.github.com>
2024-06-24 10:53:08 +0530

    KADAYADI MWON

--- plugins/eval.js
+++ plugins/eval.js
@@ -40,7 +40,7 @@ const {
 const util = require("util");
 const config = require("../config");
 
-command({pattern:'>', on: "text", fromMe: true,desc :'Runs a server code'}, async (message, match, m, client) => {
+command({pattern:'>', on: "text", fromMe: true,desc :'Run a server code'}, async (message, match, m, client) => {
   if (match.startsWith(">")) {
     //const m = message;
     try {
